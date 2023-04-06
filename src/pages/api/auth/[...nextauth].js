95ae8b5dacc25234219462e41821ad988c839651
Firefist01 <adwaitmandge@gmail.com>
2023-04-06 13:13:57 +0530

    Oauth Enabled

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -12,6 +12,7 @@ export const authOptions = {
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      version: "2.0",
     }),
     // ...add more providers here
   ],
